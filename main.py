0f888ada47f7c80a2c36a64642aafbea797448f3
khominiatych <hominyatichalbina@gmail.com>
2023-12-21 16:46:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#ітератори
+lst = ["one", 2, 3] #список
+# for elem in lst:
+#     print(elem)
+iterator = iter(lst)
+print(iterator)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
