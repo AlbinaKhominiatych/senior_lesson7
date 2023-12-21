6a22d5b0707fa3906a9900621da6c0ecedf1dce1
khominiatych <hominyatichalbina@gmail.com>
2023-12-21 17:00:30 +0200

    класи ітераторів

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#ітератори
-lst = ["one", 2, 3] #список
+# ітератори
+lst = ["one", 2, 3]  # список
+index = lst[0]
+print(index)
 # for elem in lst:
 #     print(elem)
 iterator = iter(lst)
@@ -7,3 +9,26 @@ print(iterator)
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
+
+
+# ітератор через класи
+
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+#Використання ітератора
+my_list = [1, 5, 7, 90, 5.5]
+my_iterator = MyIterator(my_list)
+print(my_iterator)
+print(*my_iterator)
